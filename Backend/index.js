af0afbfe4464b2fef931471e0589b3c4f8d9a6c8
CoderRoyshub <royshubham2401@gmail.com>
2025-12-26 01:39:20 +0530

    final commit

--- Backend/index.js
+++ Backend/index.js
@@ -7,15 +7,11 @@ const userRoutes = require("./routes/userRoutes.js")
 const app = express()
 app.use(express.json());
 app.use(cors());
+connectedDB();
 app.use("/api/users", userRoutes)
 const PORT = process.env.PORT
 
-connectedDB.then(()=>{
-    app.listen(PORT , ()=>{
+app.listen(PORT , ()=>{
     console.log(`Server is Connected on ${PORT}`)
 })
-}).catch((err) =>{
-    console.log(`Server is not working`,err)
-})
-
 
